e49d68722a2760520c046820a3d94c16af89f176
Vugar Safarzada <50525576+vugarsafarzada@users.noreply.github.com>
2024-03-26 23:32:55 +0400

    Sending email system

--- assets/helpers/emailHelper.js
+++ assets/helpers/emailHelper.js
@@ -6,34 +6,34 @@ export function $sendEmail(to) {
     return {
         '@noreply': {
             async resetPassword(payload) {
-                console.log(`[@] "Reset Password" email request: (${SMTPAddress.noreply.email} -> ${to})`);
                 // -----------------------------------------------------------------
-                const info = await SMTPAddress.noreply.transporter.sendMail({
-                    from: `${appDomain} <${SMTPAddress.noreply.email}>`, // sender address
-                    to, // list of receivers
-                    subject: "Reset Password!", // Subject line
-                    text: "Hello there, welcome to Faynn. Please Confirm your email address!", // plain text body
-                    html: "<div><h1>Reset Password!</h1></div>"// html body
+                const email = await SMTPAddress.noreply.transporter.sendMail({
+                    from: SMTPAddress.noreply.default_from,
+                    to,
+                    subject: "Reset Password!",
+                    text: "Hello there, welcome to Faynn. Please Confirm your email address!",
+                    html: "<div><h1>Reset Password!</h1></div>"
                 });
 
-                console.log("Message sent: %s", info.response);
-                // -----------------------------------------------------------------                return null;
+                console.log(`[@] "Reset Password" email request: (${SMTPAddress.noreply.email} -> ${to})`);
+                console.log("Email sent: %s", email.response);
+                // -----------------------------------------------------------------
+                return email;
             },
             async confirmEmail(payload) {
-                console.log(`[@] "Confirm Email" email request: (${SMTPAddress.noreply.email} -> ${to})`);
                 // -----------------------------------------------------------------
-                const info = await SMTPAddress.noreply.transporter.sendMail({
-                    from: `${appDomain} <${SMTPAddress.noreply.email}>`, // sender address
-                    to, // list of receivers
-                    subject: "Confirm Email!", // Subject line
-                    text: "Hello there, welcome to Faynn. Please Confirm your email address!", // plain text body
-                    html: "<b>Hello world?</b>", // html body
+                const email = await SMTPAddress.noreply.transporter.sendMail({
+                    from: `${appDomain} <${SMTPAddress.noreply.email}>`,
+                    to,
+                    subject: "Confirm Email!",
+                    text: "Hello there, welcome to Faynn. Please Confirm your email address!",
+                    html: "<b>Hello world?</b>",
                 });
 
-                console.log("Message sent: %s", info.response);
+                console.log(`[@] "Confirm Email" email request: (${SMTPAddress.noreply.email} -> ${to})`);
+                console.log("Email sent: %s", email.response);
                 // -----------------------------------------------------------------
-
-                return info;
+                return email;
             },
         },
     }
--- configs/emailConfigs.js
+++ configs/emailConfigs.js
@@ -2,11 +2,12 @@ import 'dotenv/config';
 import nodemailer from "nodemailer";
 
 let appDomain = process.env.APP_BRAND_DOMAIN.toLowerCase();
-
+let appName = process.env.APP_BRAND_NAME;
 export const SMTPAddress = {
     noreply: {
         //Typically used for automated emails where recipients are not expected to reply, such as newsletters, notifications, or confirmation emails.
         email: `noreply@${appDomain}`,
+        default_from: `${appName} <noreply@${appDomain}>`,
         transporter: nodemailer.createTransport({
             host: process.env.SMTP_SERVER,
             port: 587,
@@ -20,147 +21,73 @@ export const SMTPAddress = {
     info: {
         //Used for general inquiries, providing an easy-to-remember address for potential clients or partners.
         email: `info@${appDomain}`,
+        default_from: `${appName} <info@${appDomain}>`,
         transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
+            host: process.env.SMTP_SERVER,
+            port: 587,
             secure: false,
             auth: {
-                user: null,
-                pass: null
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
             }
         })
     },
     support: {
         //Dedicated to customer support, helping users with their issues or questions regarding products or services.
         email: `support@${appDomain}`,
+        default_from: `${appName} Support Team <support@${appDomain}>`,
         transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
+            host: process.env.SMTP_SERVER,
+            port: 587,
             secure: false,
             auth: {
-                user: null,
-                pass: null
-            }
-        })
-    },
-    sales: {
-        //Focused on sales inquiries, where potential customers can get more information about products or services.
-        email: `sales@${appDomain}`,
-        transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
-            secure: false,
-            auth: {
-                user: null,
-                pass: null
-            }
-        })
-    },
-    hr: {
-        //For human resources, where job applicants or employees can send their resumes or inquire about HR-related matters.
-        email: `hr@${appDomain}`,
-        transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
-            secure: false,
-            auth: {
-                user: null,
-                pass: null
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
             }
         })
     },
     admin: {
         //Used by the administrative department for various organizational tasks and communications.
         email: `admin@${appDomain}`,
+        default_from: `Admin of ${process.env.APP_BRAND_DOMAIN} <admin@${appDomain}>`,
         transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
+            host: process.env.SMTP_SERVER,
+            port: 587,
             secure: false,
             auth: {
-                user: null,
-                pass: null
-            }
-        })
-    },
-    marketing: {
-        //For the marketing team, where they handle marketing-related communications, campaigns, and inquiries.
-        email: `marketing@${appDomain}`,
-        transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
-            secure: false,
-            auth: {
-                user: null,
-                pass: null
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
             }
         })
     },
     billing: {
         //For finance or billing-related queries, where customers can ask about invoices, payments, or account details.
         email: `billing@${appDomain}`,
+        default_from: `${appName} Billing Service <billing@${appDomain}>`,
         transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
+            host: process.env.SMTP_SERVER,
+            port: 587,
             secure: false,
             auth: {
-                user: null,
-                pass: null
-            }
-        })
-    },
-    tech: {
-        //For technical support or IT-related issues, where users can report problems or seek assistance with technical products.
-        email: `tech@${appDomain}`,
-        transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
-            secure: false,
-            auth: {
-                user: null,
-                pass: null
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
             }
         })
     },
     feedback: {
         //For collecting customer feedback, suggestions, or complaints to improve services or products.
         email: `feedback@${appDomain}`,
+        default_from: `${appName} Feedback Service <feedback@${appDomain}>`,
         transporter: nodemailer.createTransport({
-            host: null,
-            port: null,
+            host: process.env.SMTP_SERVER,
+            port: 587,
             secure: false,
             auth: {
-                user: null,
-                pass: null
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
             }
         })
     },
 };
 
-const emailTemplates = {}
-
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     secure: false, // Use `true` for port 465, `false` for all other ports
-//     auth: {
-//         user: 'kaleb58@ethereal.email',
-//         pass: 'nP3yf2b2YwazgnmVvj'
-//     }
-// });
-//
-// // async..await is not allowed in global scope, must use a wrapper
-// async function main() {
-//     // send mail with defined transport object
-//     const info = await transporter.sendMail({
-//         from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-//         to: "vuqarsfrzad@gmail.com, kixeha3161@shaflyn.com", // list of receivers
-//         subject: "Hello ✔", // Subject line
-//         text: "Hello world?", // plain text body
-//         html: "<b>Hello world?</b>", // html body
-//     });
-//
-//     console.log("Message sent: %s", info.messageId);
-//     // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-// }
-
-// main().catch(console.error); // for send email
+const emailTemplates = {};
