cfd5778a79818750420142e1412920f4552187f9
Vugar Safarzada <vuqarsfrzad@gmail.com>
2024-04-27 14:47:22 +0400

    Your password updated email

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ import dotenv from 'dotenv';
 import database from "./configs/sequelizeConfig.js";
 import HttpCodes from "./assets/helpers/statusCodes.js";
 import sync from "./db/sync.js";
+import useragent from "express-useragent";
 
 dotenv.config();
 const app = express();
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(useragent.express());
 
 // #AREA - for accept post request data within the request body
 app.use(express.urlencoded({ extended: false }));
