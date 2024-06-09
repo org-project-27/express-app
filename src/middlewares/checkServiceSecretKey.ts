0b8879341bdca9308e58324586dd66dd4b73e924
Vugar Safarzada <vuqarsfrzad@gmail.com>
2024-06-09 04:51:11 +0400

    Update X_SECRET_KEY control system

--- src/middlewares/checkServiceSecretKey.ts
+++ src/middlewares/checkServiceSecretKey.ts
@@ -1,16 +1,28 @@
 import {NextFunction, Request, Response} from "express";
 import dotenv from "dotenv";
+import {$logged} from "#helpers/generalHelpers";
 
 dotenv.config();
 export default (req: Request, res: Response, next: NextFunction) => {
     if(!process.env.DEVELOPER_MODE){
         const secretKey = req.headers['x-secret-key'];
-
         if (!secretKey) {
+            $logged(
+                '⛔️ Unauthenticated request'.toUpperCase(),
+                false,
+                {from: 'X_SECRET_KEY', file: __filename.split('/src')[1]},
+                req.ip
+            )
             return res.status(401).json({ error: 'Service auth required' });
         }
 
         if (secretKey !== process.env.X_SECRET_KEY) {
+            $logged(
+                '⛔️ Request with invalid secret key'.toUpperCase(),
+                false,
+                {from: 'X_SECRET_KEY', file: __filename.split('/src')[1]},
+                req.ip
+            )
             return res.status(403).json({ error: 'Invalid secret key' });
         }
 
