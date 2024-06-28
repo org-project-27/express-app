47a5b18e3d0d85c5f7fe0f80f20126448b897d0d
Mehdi Safarzade <seferzademehdi@gmail.com>
2024-06-29 00:06:57 +0400

    Upload service update

--- src/assets/helpers/methods.ts
+++ src/assets/helpers/methods.ts
@@ -1,50 +1,88 @@
-import statusCodes from "../constants/statusCodes";
-import createError from "http-errors";
-import dotenv from "dotenv";
-import apiMessageKeys from "../constants/apiMessageKeys";
+import statusCodes from '../constants/statusCodes';
+import createError from 'http-errors';
+import dotenv from 'dotenv';
+import apiMessageKeys from '../constants/apiMessageKeys';
 import { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
 let responseDelay: number = Number(process.env.RESPONSE_DELAY || 0);
 
 export const $sendResponse = {
-  success: (
-    data = {},
-    via: Response,
-    message = apiMessageKeys.DONE,
-    statusCode = statusCodes.OK,
-    other = {}
-  ) => {
-    const tId = setTimeout(() => {
-      via.status(statusCode).send({ success: true, status: statusCode, message, data, ...other });
-      clearTimeout(tId);
-    }, responseDelay);
-  },
-  failed: (
-    data = {},
-    via: Response,
-    message = apiMessageKeys.SOMETHING_WENT_WRONG,
-    statusCode = statusCodes.BAD_REQUEST,
-  ) => {
-    const tId = setTimeout(() => {
-      via.status(statusCode).send({ success: false, status: statusCode, message, ...data});
-      clearTimeout(tId);
-    }, responseDelay);
-  },
+    success: (data = {}, via: Response, message = apiMessageKeys.DONE, statusCode = statusCodes.OK, other = {}) => {
+        const tId = setTimeout(() => {
+            via.status(statusCode).send({ success: true, status: statusCode, message, data, ...other });
+            clearTimeout(tId);
+        }, responseDelay);
+    },
+    failed: (
+        data = {},
+        via: Response,
+        message = apiMessageKeys.SOMETHING_WENT_WRONG,
+        statusCode = statusCodes.BAD_REQUEST
+    ) => {
+        const tId = setTimeout(() => {
+            via.status(statusCode).send({ success: false, status: statusCode, message, ...data });
+            clearTimeout(tId);
+        }, responseDelay);
+    }
 };
 
 export const $callToAction = (controller: any) => {
-  return (req: Request, res: Response, next: NextFunction | never) => {
-    const { actions } = new controller(req, res);
-    if (!!actions && !!req && !!res) {
-      const { method, path } = req;
-      if (actions[method] && actions[method][path]) {
-        actions[method][path](req, res, next);
-      } else {
-        next(createError(statusCodes.NOT_FOUND));
-      }
-    } else {
-      next(createError(statusCodes.INTERNAL_SERVER_ERROR));
-    }
-  };
+    return (req: Request, res: Response, next: NextFunction | never) => {
+        const { actions } = new controller(req, res);
+        if (!!actions && !!req && !!res) {
+            const { method, path } = req;
+            if (actions[method] && actions[method][path]) {
+                actions[method][path]({ req, res, next });
+                // if (path.includes('/:')) {
+                //     let params: Record<string, any> = {};
+
+                //     path.split('/').forEach((p, i) => {
+                //         if (p.startsWith(':')) {
+                //             params[p.replace(':', '')] = req.params[i];
+                //         }
+                //     });
+
+                //     actions[method][path]({ req, res, next, params });
+                // } else {
+                //     actions[method][path]({ req, res, next });
+                // }
+            } else {
+                let originPath = '';
+                let reqParams: Record<string, any> = {};
+
+                for (let p in actions[method]) {
+                    if (p.includes('/:')) {
+                        let pathParts = p.split('/');
+                        let reqParts = path.split('/');
+
+                        if (pathParts.length === reqParts.length) {
+                            let isMatched = true;
+                            let params: Record<string, any> = {};
+
+                            pathParts.forEach((part, i) => {
+                                if (part.startsWith(':')) {
+                                    params[part.replace(':', '')] = reqParts[i];
+                                } else if (part !== reqParts[i]) {
+                                    isMatched = false;
+                                }
+                            });
+
+                            if (isMatched) {
+                                originPath = p;
+                                reqParams = params;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (originPath) {
+                    actions[method][originPath]({ req, res, next, params: reqParams });
+                } else next(createError(statusCodes.NOT_FOUND));
+            }
+        } else {
+            next(createError(statusCodes.INTERNAL_SERVER_ERROR));
+        }
+    };
 };
--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -6,99 +6,247 @@ import { Request, Response, NextFunction } from 'express';
 import { $callToAction, $sendResponse } from '#helpers/methods';
 import apiMessageKeys from '~/assets/constants/apiMessageKeys';
 import multer from 'multer';
-import statusCodes from "#assets/constants/statusCodes";
+import statusCodes from '#assets/constants/statusCodes';
 
 class UploadController extends Controller {
-  constructor(request: Request, response: Response) {
-    super(request, response);
-    this.actions['POST']['/example'] = this.example;
-  }
-  public example = async () => {
-    try {
-      const object_id = this.reqBody.object_id;
-      const object = await this.database.objects.findUnique({
-        where: { id: object_id },
-      });
-
-      if (!object) {
-        $logged(`Object not found: ${object_id}`, false, { from: 'upload', object_id });
-        return $sendResponse.failed({}, this.response, apiMessageKeys.SOMETHING_WENT_WRONG, 500);
-      }
-
-      $sendResponse.success(
-          {
-            filename: object.name,
-            path: `http://localhost:5501/cdn/${object_id}`,
-          },
-          this.response
-      );
-    } catch (error) {
-      $logged(
-          `Object uploading progress failed\n${error}`,
-          false,
-          { file: __filename.split('/src')[1], payload: this.reqBody },
-          this.request.ip
-      );
-      return $sendResponse.failed(
-          { error },
-          this.response,
-          apiMessageKeys.SOMETHING_WENT_WRONG,
-          statusCodes.INTERNAL_SERVER_ERROR
-      );
+    constructor(request: Request, response: Response) {
+        super(request, response);
+        this.actions['POST']['/upload'] = this.upload;
+        this.actions['PUT']['/update/:id'] = this.update;
+        this.actions['DELETE']['/delete/:id'] = this.delete;
     }
-  };
+    public example = async () => {
+        try {
+            const object_id = this.reqBody.object_id;
+            const object = await this.database.objects.findUnique({
+                where: { id: object_id }
+            });
+
+            if (!object) {
+                $logged(`Object not found: ${object_id}`, false, { from: 'upload', object_id });
+                return $sendResponse.failed({}, this.response, apiMessageKeys.SOMETHING_WENT_WRONG, 500);
+            }
+
+            $sendResponse.success(
+                {
+                    filename: object.name,
+                    path: `http://localhost:5501/cdn/${object_id}`
+                },
+                this.response
+            );
+        } catch (error) {
+            $logged(
+                `Object uploading progress failed\n${error}`,
+                false,
+                { file: __filename.split('/src')[1], payload: this.reqBody },
+                this.request.ip
+            );
+            return $sendResponse.failed(
+                { error },
+                this.response,
+                apiMessageKeys.SOMETHING_WENT_WRONG,
+                statusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    };
+
+    public upload = async () => {
+        try {
+            const object_id = this.reqBody.object_id;
+            const object = await this.database.objects.findUnique({
+                where: { id: object_id }
+            });
+
+            if (!object) {
+                $logged(`Object not found: ${object_id}`, false, { from: 'uploader/upload', object_id });
+                return $sendResponse.failed(
+                    {},
+                    this.response,
+                    apiMessageKeys.SOMETHING_WENT_WRONG,
+                    statusCodes.INTERNAL_SERVER_ERROR
+                );
+            }
+
+            $sendResponse.success(
+                {
+                    filename: object.name,
+                    path: `http://localhost:5501/cdn/${object_id}`
+                },
+                this.response
+            );
+        } catch (error) {
+            $logged(
+                `Object uploading progress failed\n${error}`,
+                false,
+                { file: __filename.split('/src')[1], payload: this.reqBody },
+                this.request.ip
+            );
+            return $sendResponse.failed(
+                { error },
+                this.response,
+                apiMessageKeys.SOMETHING_WENT_WRONG,
+                statusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    };
+
+    public update = async ({ params }: { params: Record<string, any> }) => {
+        try {
+            const id = params.id;
+            const object_id = this.reqBody.object_id;
+            const { user_id } = this.reqBody.authentication_result.payload;
+
+            if (!id) {
+                $logged(`Object not found: ${object_id}`, false, { from: 'upload/update', object_id });
+                return $sendResponse.failed({}, this.response, apiMessageKeys.PARAM_REQUIRED, statusCodes.BAD_REQUEST);
+            }
+
+            const oldObject = await this.database.objects.findUnique({
+                where: { id, user_id }
+            });
+
+            if (!oldObject) {
+                $logged(`Object not found: ${id}`, false, { from: 'upload/update', object_id });
+                return $sendResponse.failed({}, this.response, apiMessageKeys.OBJECT_NOT_FOUND, statusCodes.NOT_FOUND);
+            }
+
+            const newObject = await this.database.objects.findUnique({
+                where: { id: object_id }
+            });
+
+            if (!newObject) {
+                $logged(`Object not found: ${object_id}`, false, { from: 'upload/update', object_id });
+                return $sendResponse.failed(
+                    {},
+                    this.response,
+                    apiMessageKeys.SOMETHING_WENT_WRONG,
+                    statusCodes.INTERNAL_SERVER_ERROR
+                );
+            }
+
+            const oldPath = oldObject.path;
+            this.cdn.deleteObject(oldPath);
+            await this.database.objects.delete({
+                where: { id }
+            });
+
+            $sendResponse.success(
+                {
+                    filename: newObject.name,
+                    path: `http://localhost:5501/cdn/${object_id}`
+                },
+                this.response
+            );
+        } catch (error) {
+            $logged(
+                `Object updating progress failed\n${error}`,
+                false,
+                { file: __filename.split('/src')[1], payload: this.reqBody },
+                this.request.ip
+            );
+            return $sendResponse.failed(
+                { error },
+                this.response,
+                apiMessageKeys.SOMETHING_WENT_WRONG,
+                statusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    };
+
+    public delete = async ({ params }: { params: Record<string, any> }) => {
+        try {
+            const id = params.id;
+            const { user_id } = JSON.parse(this.reqBody.authentication_result);
+
+            if (!id) {
+                $logged(`Object not found: ${id}`, false, { from: 'uploader/delete', id });
+                return $sendResponse.failed({}, this.response, apiMessageKeys.PARAM_REQUIRED, statusCodes.BAD_REQUEST);
+            }
+
+            const object = await this.database.objects.findUnique({
+                where: { id, user_id }
+            });
+
+            if (!object) {
+                $logged(`Object not found: ${id}`, false, { from: 'upload', id });
+                return $sendResponse.failed({}, this.response, apiMessageKeys.OBJECT_NOT_FOUND, statusCodes.NOT_FOUND);
+            }
+
+            const path = object.path;
+            this.cdn.deleteObject(path);
+            await this.database.objects.delete({
+                where: { id }
+            });
+
+            $sendResponse.success({}, this.response);
+        } catch (error) {
+            $logged(
+                `Object deleting progress failed\n${error}`,
+                false,
+                { file: __filename.split('/src')[1], payload: this.reqBody },
+                this.request.ip
+            );
+            return $sendResponse.failed(
+                { error },
+                this.response,
+                apiMessageKeys.SOMETHING_WENT_WRONG,
+                statusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    };
 }
 
 export function uploader(request: Request, response: Response, next: NextFunction) {
-  try {
-    const authentication_result = JSON.parse(request.body.authentication_result);
-    const { user_id } = authentication_result.payload;
-    const database = new PrismaClient();
-    const storage = multer.diskStorage({
-      destination: function (req, file, cb) {
-        const { mimetype } = file;
-        const fileType = mimetype.split('/')[0];
-        console.log(fileType);
-        const category = ['image', 'video', 'audio'].includes(fileType) ? fileType : 'other';
-        const path = `cdn/${category}/`;
+    try {
+        const authentication_result = JSON.parse(request.body.authentication_result);
+        const { user_id } = authentication_result.payload;
+        const database = new PrismaClient();
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                const { mimetype } = file;
+                const fileType = mimetype.split('/')[0];
+                console.log(fileType);
+                const category = ['image', 'video', 'audio'].includes(fileType) ? fileType : 'other';
+                const path = `cdn/${category}/`;
 
-        cb(null, path);
-      },
-      filename: async function (req, file, cb) {
-        const { originalname, mimetype } = file;
-        const extension = originalname.split('.').pop();
-        const path = `${Date.now()}.${extension}`;
-        const id = uuidv4();
-        await database.objects.create({
-          data: {
-            id,
-            name: originalname,
-            type: mimetype,
-            path,
-            user_id: user_id,
-          },
+                cb(null, path);
+            },
+            filename: async function (req, file, cb) {
+                const { originalname, mimetype } = file;
+                const extension = originalname.split('.').pop();
+                const id = uuidv4();
+                const path = `${id}.${extension}`;
+                await database.objects.create({
+                    data: {
+                        id,
+                        name: originalname,
+                        type: mimetype,
+                        path,
+                        user_id: user_id
+                    }
+                });
+
+                req.body['authentication_result'] = authentication_result;
+                req.body['object_id'] = id;
+                cb(null, path);
+            }
         });
-
-        req.body['object_id'] = id;
-        cb(null, path);
-      },
-    });
-    const upload = multer({ storage });
-    return upload.single('file')(request, response, next);
-  } catch (error) {
-    $logged(
-        `Object uploading progress failed\n${error}`,
-        false,
-        { file: __filename.split('/src')[1], payload: request.body },
-        request.ip
-    );
-    return $sendResponse.failed(
-        { error },
-        response,
-        apiMessageKeys.SOMETHING_WENT_WRONG,
-        statusCodes.INTERNAL_SERVER_ERROR
-    );
-  }
+        const upload = multer({ storage });
+        return upload.single('file')(request, response, next);
+    } catch (error) {
+        $logged(
+            `Multer uploading progress failed\n${error}`,
+            false,
+            { file: __filename.split('/src')[1], payload: request.body },
+            request.ip
+        );
+        return $sendResponse.failed(
+            { error },
+            response,
+            apiMessageKeys.SOMETHING_WENT_WRONG,
+            statusCodes.INTERNAL_SERVER_ERROR
+        );
+    }
 }
 
 export default $callToAction(UploadController);
