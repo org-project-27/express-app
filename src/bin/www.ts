196a8843824ba86a4e224c5f55989e69b416dcca
Vugar Safarzada <vuqarsfrzad@gmail.com>
2024-07-01 01:38:12 +0400

    Filter TgBot logs

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -40,12 +40,12 @@ function onError(error: any) {
   switch (error.code) {
     case 'EACCES':
       let log_EACCES = bind + ' requires elevated privileges'
-      $logged(log_EACCES, false);
+      $logged(log_EACCES, false, {from: 'node.js'}, null, true);
       process.exit(1);
       break;
     case 'EADDRINUSE':
       let log_EADDRINUSE = bind + ' is already in use';
-      $logged(log_EADDRINUSE, false);
+      $logged(log_EADDRINUSE, false, {from: 'node.js'}, null, true);
       process.exit(1);
       break;
     default:
@@ -60,7 +60,7 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
   let log = `Express app running on ${addr.address}${addr.port}`
-  $logged(log, true);
+  $logged(log, true, {from: 'node.js'}, null, true);
 }
 
 const server = http.createServer(app);
