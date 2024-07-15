4b5019e3e3244fe3cd9a985313b30e7b52475934
Mehdi Safarzade <seferzademehdi@gmail.com>
2024-07-15 14:28:32 +0400

    New function and bug fixes

--- src/assets/constants/general.ts
+++ src/assets/constants/general.ts
@@ -1,4 +1,9 @@
-export const ASCII_logo = `||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+import { config } from "dotenv";
+import { existsSync, mkdir, writeFile } from "fs";
+config();
+
+export const ASCII_logo = `
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 ||                                                          ||
 ||   //////  //   //  /////   /////   /////  /////  /////   ||
 ||   //       // //   //  //  //  //  //     //     //      ||
@@ -9,5 +14,23 @@ export const ASCII_logo = `|||||||||||||||||||||||||||||||||||||||||||||||||||||
 ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 `;
 
-export const currentLogFilePath = 'src/bin/logs/logs.txt';
-export const logsBasePath = 'src/bin/logs';
+const isDev = process.env.NODE_ENV === 'development';
+
+export const currentLogFilePath = isDev ? 'src/bin/logs/logs.txt' : 'dist/bin/logs/logs.txt';
+export const logsBasePath = isDev ? 'src/bin/logs': 'dist/bin/logs';
+
+if (!existsSync(logsBasePath)) {
+    mkdir(logsBasePath, { recursive: true }, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
+if (!existsSync(currentLogFilePath)) {
+    writeFile(currentLogFilePath, '', (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
